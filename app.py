b2e3e6970a6cd1c03fc34bfede698feda359ebd9
Thanakorn Ritsub <6531501191@lamduan.mfu.ac.th>
2025-04-20 11:22:39 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 20 10:44:07 2025
+
+@author: pangp
+"""
+
+#import library
+import streamlit as st
+import numpy as np
+import pickle
+
+#load model
+with open('dtm_trained_moel.pkl','rb') as f:
+    dtm_model = pickle.load(f)
+    
+#application title
+st.title("Iris flower Classificartion")
+st.write("Enter the feature of the iris flower :")
+
+sepal_length = st.slider("Sepal Length (cm)" ,4,0,8,0,5,1)
+sepal_width = st.slider("Sepal Width (cm)" ,2,0,4,5,3,5)
+petal_length = st.slider("Petal Length (cm)" ,1.0,7.0,1.4)
+petal_width = st.slider("Petal Width (cm)" ,0.1,2.5,0.2)
+
+if st.button("Predict"):
+    input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    prediction = dtm_model.predict(input_data)
+    species = ['Setosa', 'Versicolor', 'Virginica']
+    st.success(f"The predicted species is: **{species[prediction[0]]}**")
